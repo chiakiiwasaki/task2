def25ece500bff9e2ee3bce92cae4996401af10b
chiakiiwasaki <chiaki103ki@gmail.com>
2021-07-01 11:42:46 +0000

    [Finish]task2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,56 @@
+class BooksController < ApplicationController
+
+  def show
+    @book = Book.find(params[:id])
+    @new_book = Book.new
+  end
+
+  def index
+    @user = current_user
+    @books = Book.all
+    @new_book = Book.new
+  end
+
+  def create
+    @new_book = Book.new(book_params)
+    @new_book.user_id = current_user.id
+    @user = current_user
+    @books = Book.all
+    if @new_book.save
+      redirect_to book_path(@new_book.id), notice: "You have created book successfully."
+    else
+      render 'index'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user != current_user
+      redirect_to books_path
+    end
+  end
+
+
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.delete
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
